c53c7d6239d31f46054b41228a6caff77fd77b27
Stephen Wong <wongstephenky@gmail.com>
2022-09-29 20:59:41 -0700

    updated styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import Contact from "./components/Contact";
+import { Footer } from "./components/Footer";
 import Header from "./components/Header";
-import NavBar from "./components/NavBar";
 import Projects from "./components/Projects";
 
 function App() {
@@ -10,17 +10,22 @@ function App() {
     ref.current?.scrollIntoView({ behavior: "smooth" });
   }
   return (
-    <div className="max-w-5xl mx-auto mb-24">
-      {/* <NavBar /> */}
+    <div className="">
       <div className="main__container">
         <Header clickEvent={handleClick} />
         <main>
-          <div className="w-11/12 h-[1px] mx-auto my-14 bg-slate-500/20"></div>
+          <div
+            className="w-11/12 h-[1px] mx-auto my-24
+           bg-slate-500/20"
+          ></div>
           <Projects />
-          <div className="w-11/12 h-[1px] mx-auto my-14 bg-slate-500/20"></div>
-
+          <div
+            className="w-11/12 h-[1px] mx-auto my-24
+           bg-slate-500/20"
+          ></div>{" "}
           <Contact refs={ref} />
         </main>
+        <Footer />
       </div>
     </div>
   );
--- src/components/presentation/ProjectItem.js
+++ src/components/presentation/ProjectItem.js
@@ -2,51 +2,57 @@ import React from "react";
 
 export const ProjectItem = ({ data }) => {
   return (
-    <li className="my-10 md:flex-grow md:basis-1/3">
+    <li className="max-w-sm my-16 md:flex-grow md:basis-1/3">
       {/* image */}
 
       <img
-        className="flex justify-center w-full mx-auto overflow-hidden align-middle border-0 rounded shadow-md max-w-max"
+        className="flex justify-center w-full mx-auto overflow-hidden align-middle border-0 shadow-md rounded-tl-xl rounded-br-xl max-w-max"
         src={data.photo}
         alt={data.title}
       />
 
+      {/* text content */}
       <div className="flex flex-col w-full gap-10 mx-auto mt-10">
         <div className="flex flex-col">
-          <p className="mb-5 text-xl font-medium font-poppins">{data.title}</p>
-          <p className="text-sm font-medium mb-2.5">
-            {data.tech} {" - "}
-            <span className="font-light ">{data.description}</span>
-          </p>
-          {data.demoCreds && (
-            <p className="p-0 m-0 text-sm">
-              Demo Login:{" "}
-              <span className="font-light text-slate-600">
-                {data.demoCreds}
-              </span>
+          <div className="md:min-h-[14rem]">
+            <p className="mb-2.5 text-2xl font-base font-poppins">
+              {data.title}
             </p>
-          )}
-          {data.demoPassword && (
-            <p className="p-0 m-0 text-sm">
-              Demo Password:{" "}
-              <span className="font-light text-slate-600">
-                {data.demoPassword}
-              </span>
+            <p className="text-sm font-medium mb-2.5 ">
+              {data.tech} {" - "}
+              <span className="font-light ">{data.description}</span>
             </p>
-          )}
+
+            {data.demoCreds && (
+              <p className="p-0 m-0 text-sm">
+                Demo Login:{" "}
+                <span className="font-light text-slate-600">
+                  {data.demoCreds}
+                </span>
+              </p>
+            )}
+            {data.demoPassword && (
+              <p className="p-0 m-0 text-sm">
+                Demo Password:{" "}
+                <span className="font-light text-slate-600">
+                  {data.demoPassword}
+                </span>
+              </p>
+            )}
+          </div>
         </div>
 
         <div className="flex gap-5">
           {data.wwwLink && (
             <a href={data.wwwLink} target="_blank" rel="noreferrer">
-              <button className="border-[1px] border-sky-600 text-sky-600 tracking-widest font-medium text-sm rounded px-5 py-1 hover:bg-sky-600/20 hover:shadow-lg">
+              <button className="px-8 py-4 mx-auto tracking-wider text-white transition-all duration-500 border rounded-lg cursor-pointer hover:bg-color2 hover:shadow bg-color1">
                 Visit
               </button>
             </a>
           )}
           {data.githubLink && (
             <a href={data.githubLink} target="_blank" rel="noreferrer">
-              <button className="border-[1px] border-sky-600 text-sm  text-sky-600 tracking-widest font-medium rounded px-5 py-1 hover:bg-sky-600/20 hover:shadow-lg">
+              <button className="px-8 py-4 mx-auto tracking-wider text-white transition-all duration-500 border rounded-lg cursor-pointer hover:bg-color2 hover:shadow bg-color1">
                 Github
               </button>
             </a>
